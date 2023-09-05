ad235fba965f6b52c55aa6093147ebb246caa853
Christian Tamblyn <ctamblyn76@gmail.com>
2023-09-05 10:00:34 +0100

    Fix clippy warnings.

--- benches/benchmarks.rs
+++ benches/benchmarks.rs
@@ -5,7 +5,7 @@ fn simple_expansion(c: &mut Criterion) {
     let input = "aaa{bbb,ccc}ddd";
 
     c.bench_function("simple expansion", |b| {
-        b.iter(|| brace_expand(black_box(&input)))
+        b.iter(|| brace_expand(black_box(input)))
     });
 }
 
@@ -13,7 +13,7 @@ fn nested_expansion(c: &mut Criterion) {
     let input = "{aaa,bbb}ccc{eee,fff{ggg,hhh}}";
 
     c.bench_function("nested expansion", |b| {
-        b.iter(|| brace_expand(black_box(&input)))
+        b.iter(|| brace_expand(black_box(input)))
     });
 }
 
